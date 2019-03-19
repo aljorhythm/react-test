247755d0fcef9ae1908db8c289a1eae5d4df64a4
Joel Lim <103879u@gmail.com>
2019-03-20 03:35:02 +0800

    edit components and containers

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ListGroupItem } from 'reactstrap';
+import { ListGroupItem, Button } from 'reactstrap';
 
-const Todo = ({ onClick, completed, text }) => (
+const DeleteTodoButton = ({onClick}) => (
+  <Button onClick={onClick}>X</Button>
+);
+
+const Todo = ({ onClick, onDeleteClick, completed, text }) => (
   <ListGroupItem
     onClick={onClick}
     style={{
-      textDecoration: completed ? 'line-through' : 'none'
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between'
     }}
   >
-    {text}
+    <span style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}>
+      {text}    
+    </span>
+    <DeleteTodoButton onClick={onDeleteClick}/>
   </ListGroupItem>
 )
 
 Todo.propTypes = {
   onClick: PropTypes.func.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import { ListGroup } from 'reactstrap';
 import Todo from './Todo';
 
-const TodoList = ({ todos, toggleTodo }) => (
+const TodoList = ({ todos, toggleTodo, deleteTodo }) => (
   <ListGroup style={{ marginTop: 10, marginBottom: 10 }}>
     {todos.map(todo =>
       <Todo
         key={todo.id}
         {...todo}
         onClick={() => toggleTodo(todo.id)}
+        onDeleteClick={() => deleteTodo(todo.id)}
       />
     )}
   </ListGroup>
